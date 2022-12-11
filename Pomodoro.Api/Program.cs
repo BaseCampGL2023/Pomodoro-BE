80f5bc8f3f8fba9055d32e891df871eddde49048
AnatoliyF <anatoliyf89@gmail.com>
2022-12-11 19:24:06 +0200

    setup webAPI

--- Pomodoro.Api/Program.cs
+++ Pomodoro.Api/Program.cs
@@ -1,15 +1,60 @@
+using Microsoft.OpenApi.Models;
+
+var PomodoroSpecificOrigins = "_pomodoroSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: PomodoroSpecificOrigins,
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200");
+            policy.AllowAnyHeader();
+            policy.AllowAnyMethod();
+        });
+});
+
+//setup Serilog
+//builder.Host.UseSerilog((ctx, lc) => lc
+//    .ReadFrom.Configuration(ctx.Configuration)
+//    .WriteTo.MSSqlServer(connectionString:
+//        ctx.Configuration.GetConnectionString("PomodoroBE"),
+//        restrictedToMinimumLevel: LogEventLevel.Information,
+//        sinkOptions: new MSSqlServerSinkOptions
+//        {
+//            TableName = "LogEvents",
+//            AutoCreateSqlTable = true,
+//        }
+//        )
+//    .WriteTo.Console()
+//);
+
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Pomodoro",
+        Version = "v1",
+        Description = "Time tracker"
+    });
+    option.EnableAnnotations();
+});
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+//uncomment, if want logging HTTP requests
+//app.UseSerilogRequestLogging();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,6 +62,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors(PomodoroSpecificOrigins);
 
 app.UseAuthorization();
 
