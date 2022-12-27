8297ce5ddc88bac484107fc0a84c18920a886018
Сігута Діана <diana.sihuta@nure.ua>
2022-12-27 15:39:55 +0200

    Add extension method to let AppDbContext be registered as a service

---
+++ Pomodoro.DataAccess/Extensions/AppDbContextExtension.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="AppDbContextExtension.cs" company="PomodoroGroup_GL_BaseCamp">
+// Copyright (c) PomodoroGroup_GL_BaseCamp. All rights reserved.
+// </copyright>
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Pomodoro.DataAccess.EF;
+
+namespace Pomodoro.DataAccess.Extensions
+{
+    public static class AppDbContextExtension
+    {
+        public static IServiceCollection AddAppDbContext(
+            this IServiceCollection services,
+            string connectionString)
+        {
+            return services.AddDbContext<AppDbContext>(options => {
+                options.UseSqlServer(connectionString);
+            });
+        }
+    }
+}
