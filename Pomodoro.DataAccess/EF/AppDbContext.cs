a6c3f9e0842731a9004179545004a76322415b41
Сігута Діана <diana.sihuta@nure.ua>
2022-12-27 15:22:15 +0200

    Add AppDbContext and its factory to enable design-time services like Migrations

---
+++ Pomodoro.DataAccess/EF/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="AppDbContext.cs" company="PomodoroGroup_GL_BaseCamp">
+// Copyright (c) PomodoroGroup_GL_BaseCamp. All rights reserved.
+// </copyright>
+
+using Microsoft.EntityFrameworkCore;
+using Pomodoro.DataAccess.Entities;
+
+namespace Pomodoro.DataAccess.EF
+{
+    internal class AppDbContext : DbContext
+    {
+        public DbSet<Completed> CompletedTasks => Set<Completed>();
+        public DbSet<Frequency> Frequencies => Set<Frequency>();
+        public DbSet<FrequencyType> FrequencyTypes => Set<FrequencyType>();
+        public DbSet<Settings> Settings => Set<Settings>();
+        public DbSet<Entities.Task> Tasks => Set<Entities.Task>();
+        public DbSet<User> Users => Set<User>();
+
+        public AppDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
+    }
+}
---
+++ Pomodoro.DataAccess/EF/AppDbContextFactory.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="AppDbContextFactory.cs" company="PomodoroGroup_GL_BaseCamp">
+// Copyright (c) PomodoroGroup_GL_BaseCamp. All rights reserved.
+// </copyright>
+
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Pomodoro.DataAccess.EF
+{
+    internal class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var connectionString = GetConnectionString("LocalDB");
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+
+        private static string GetConnectionString(string connStrName)
+        {
+            var configuration = new ConfigurationBuilder()
+               .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "..", "Pomodoro.Api"))
+               .AddJsonFile("appsettings.json")
+               .Build();
+
+            return configuration.GetConnectionString(connStrName);
+        }
+    }
+}
