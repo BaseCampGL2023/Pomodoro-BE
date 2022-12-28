1416cab4c28f1e490cb8281b4f621fb28f2ba82b
Сігута Діана <diana.sihuta@nure.ua>
2022-12-28 11:49:55 +0200

    Remove adding test data to db

--- Pomodoro.DataAccess/Extensions/ModelBuilderExtension.cs
+++ Pomodoro.DataAccess/Extensions/ModelBuilderExtension.cs
@@ -14,107 +14,94 @@ namespace Pomodoro.DataAccess.Extensions
         {
             builder.SeedFrequencyTypes();
             builder.SeedFrequencies();
-            builder.SeedUsers();
-            builder.SeedSettings();
-            builder.SeedTasks();
-            builder.SeedCompletedTasks();
         }
 
         private static void SeedFrequencyTypes(this ModelBuilder builder)
         {
             builder.Seed(
-                new FrequencyType(id: 1, value: FrequencyValue.None),
-                new FrequencyType(id: 2, value: FrequencyValue.Day),
-                new FrequencyType(id: 3, value: FrequencyValue.Week),
-                new FrequencyType(id: 4, value: FrequencyValue.Month),
-                new FrequencyType(id: 5, value: FrequencyValue.Year),
-                new FrequencyType(id: 6, value: FrequencyValue.Workday),
-                new FrequencyType(id: 7, value: FrequencyValue.Weekend)
+                new FrequencyType
+                {
+                    Id = 1,
+                    Value = FrequencyValue.None
+                },
+                new FrequencyType
+                {
+                    Id = 2,
+                    Value = FrequencyValue.Day
+                },
+                new FrequencyType
+                {
+                    Id = 3,
+                    Value = FrequencyValue.Week
+                },
+                new FrequencyType
+                {
+                    Id = 4,
+                    Value = FrequencyValue.Month
+                },
+                new FrequencyType
+                {
+                    Id = 5,
+                    Value = FrequencyValue.Year
+                },
+                new FrequencyType
+                {
+                    Id = 6,
+                    Value = FrequencyValue.Workday
+                },
+                new FrequencyType
+                {
+                    Id = 7,
+                    Value = FrequencyValue.Weekend
+                }
             );
         }
 
         private static void SeedFrequencies(this ModelBuilder builder)
         {
             builder.Seed(
-                new Frequency(id: 1, frequencyTypeId: 1, every: 0),
-                new Frequency(id: 2, frequencyTypeId: 2, every: 1),
-                new Frequency(id: 3, frequencyTypeId: 3, every: 1),
-                new Frequency(id: 4, frequencyTypeId: 4, every: 1),
-                new Frequency(id: 5, frequencyTypeId: 5, every: 1),
-                new Frequency(id: 6, frequencyTypeId: 6, every: 1),
-                new Frequency(id: 7, frequencyTypeId: 7, every: 1)
-            );
-        }
-
-        private static void SeedUsers(this ModelBuilder builder)
-        {
-            builder.Seed(
-                new User(id: 1, name: "Bob", email: "bob@gmail.com"),
-                new User(id: 2, name: "Jane", email: "jane@gmail.com")
-            );
-        }
-
-        private static void SeedSettings(this ModelBuilder builder)
-        {
-            builder.Seed(
-                new Settings(
-                    id: 1, userId: 1,
-                    pomodoroDuration: 30, shortBreak: 5, longBreak: 10,
-                    pomodorosBeforeLongBreak: 2, autostartEnabled: true
-                ),
-                new Settings(
-                    id: 2, userId: 2,
-                    pomodoroDuration: 60, shortBreak: 10, longBreak: 20,
-                    pomodorosBeforeLongBreak: 3, autostartEnabled: false
-                )
-            );
-        }
-
-        private static void SeedTasks(this ModelBuilder builder)
-        {
-            builder.Seed(
-                new Entities.Task(
-                    id: 1, userId: 1, frequencyId: 1,
-                    title: "Investigate Docker", initialDate: DateTime.UtcNow,
-                    allocatedTime: 100
-                ),
-                new Entities.Task(
-                    id: 2, userId: 1, frequencyId: 3,
-                    title: "Cleaning", initialDate: DateTime.UtcNow,
-                    allocatedTime: 200
-                ),
-                new Entities.Task(
-                    id: 3, userId: 2, frequencyId: 1,
-                    title: "Generate DB", initialDate: DateTime.UtcNow,
-                    allocatedTime: 120
-                ),
-                new Entities.Task(
-                    id: 4, userId: 2, frequencyId: 2,
-                    title: "Workout", initialDate: DateTime.UtcNow,
-                    allocatedTime: 60
-                )
-            );
-        }
-
-        private static void SeedCompletedTasks(this ModelBuilder builder)
-        {
-            builder.Seed(
-                new Completed(
-                    id: 1, taskId: 1, actualDate: DateTime.UtcNow,
-                    timeSpent: 120, pomodorosCount: 4
-                ),
-                new Completed(
-                    id: 2, taskId: 2, actualDate: DateTime.UtcNow,
-                    timeSpent: 160, pomodorosCount: 5.3F
-                ),
-                new Completed(
-                    id: 3, taskId: 3, actualDate: DateTime.UtcNow,
-                    timeSpent: 100, pomodorosCount: 1.7F
-                ),
-                new Completed(
-                    id: 4, taskId: 4, actualDate: DateTime.UtcNow,
-                    timeSpent: 60, pomodorosCount: 1
-                )
+                new Frequency
+                {
+                    Id = 1,
+                    FrequencyTypeId = 1,
+                    Every = 0
+                },
+                new Frequency
+                {
+                    Id = 2,
+                    FrequencyTypeId = 2,
+                    Every = 1
+                },
+                new Frequency
+                {
+                    Id = 3,
+                    FrequencyTypeId = 3,
+                    Every = 1
+                },
+                new Frequency
+                {
+                    Id = 4,
+                    FrequencyTypeId = 4,
+                    Every = 1
+                },
+                new Frequency
+                {
+                    Id = 5,
+                    FrequencyTypeId = 5,
+                    Every = 1
+                },
+                new Frequency
+                {
+                    Id = 6,
+                    FrequencyTypeId = 6,
+                    Every = 1
+                },
+                new Frequency
+                {
+                    Id = 7,
+                    FrequencyTypeId = 7,
+                    Every = 1
+                }
             );
         }
 
