946cddf3ebdaa4fc02b79bcd2efbc7c3aebd4aa3
AnatoliyF <anatoliyf89@gmail.com>
2023-02-05 14:57:08 +0200

    add ViewModels for sign in / sign up

---
+++ Pomodoro.Api/ViewModels/Auth/RegistrationRequestViewModel.cs
@@ -0,0 +1,42 @@
+﻿// <copyright file="RegistrationRequestViewModel.cs" company="PomodoroGroup_GL_BaseCamp">
+// Copyright (c) PomodoroGroup_GL_BaseCamp. All rights reserved.
+// </copyright>
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Pomodoro.Api.ViewModels.Auth
+{
+    /// <summary>
+    /// Represent data for registration request.
+    /// </summary>
+    public class RegistrationRequestViewModel
+    {
+        /// <summary>
+        /// Gets or sets user name.
+        /// </summary>
+        [Required(ErrorMessage = "Name is requireed.")]
+        public string? UserName { get; set; }
+
+        /// <summary>
+        /// Gets or sets user email.
+        /// </summary>
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets user password.
+        /// </summary>
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+
+        /// <summary>
+        /// Gets or sets confirmed user password.
+        /// </summary>
+        [Required(ErrorMessage = "Confirmed password is required.")]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password), ErrorMessage = "Confirmation must match password.")]
+        public string? ConfirmPassword { get; set; }
+    }
+}
---
+++ Pomodoro.Api/ViewModels/Auth/RegistrationResponseViewModel.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="RegistrationResponseViewModel.cs" company="PomodoroGroup_GL_BaseCamp">
+// Copyright (c) PomodoroGroup_GL_BaseCamp. All rights reserved.
+// </copyright>
+
+namespace Pomodoro.Api.ViewModels.Auth
+{
+    /// <summary>
+    /// Represent registration request result.
+    /// </summary>
+    public class RegistrationResponseViewModel
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether registration request successful,
+        /// TRUE if successful, FALSE otherwise.
+        /// </summary>
+        public bool Success { get; set; }
+
+        /// <summary>
+        /// Gets or sets list of registration request model validation errors.
+        /// </summary>
+        public List<string>? Errors { get; set; }
+    }
+}
