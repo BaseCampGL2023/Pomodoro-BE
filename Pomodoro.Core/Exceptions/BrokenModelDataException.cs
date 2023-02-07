2febc29e04b01a719153eb6126c949981d1540fb
AnatoliyF <anatoliyf89@gmail.com>
2023-02-07 18:12:15 +0200

    Add exceptions

---
+++ Pomodoro.Core/Exceptions/BrokenModelDataException.cs
@@ -0,0 +1,14 @@
+﻿namespace Pomodoro.Core.Exceptions
+{
+
+	[Serializable]
+	public class BrokenModelDataException : PomoException
+	{
+		public BrokenModelDataException() { }
+		public BrokenModelDataException(string message) : base(message) { }
+		public BrokenModelDataException(string message, Exception inner) : base(message, inner) { }
+		protected BrokenModelDataException(
+		  System.Runtime.Serialization.SerializationInfo info,
+		  System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+	}
+}
