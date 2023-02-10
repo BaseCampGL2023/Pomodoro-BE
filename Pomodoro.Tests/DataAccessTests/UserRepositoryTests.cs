6329b0b560b9bae2845f2558349f44500211c238
unknown <asatrian.v@gmail.com>
2023-02-11 00:27:16 +0200

    add user repository tests

---
+++ Pomodoro.Tests/DataAccessTests/UserRepositoryTests.cs
@@ -0,0 +1,268 @@
+﻿// <copyright file="UserRepositoryTests.cs" company="PomodoroGroup_GL_BaseCamp">
+// Copyright (c) PomodoroGroup_GL_BaseCamp. All rights reserved.
+// </copyright>
+
+using Pomodoro.DataAccess.EF;
+using Pomodoro.DataAccess.Entities;
+using Pomodoro.DataAccess.Repositories.Realizations;
+using Pomodoro.Tests.EqualityComparers;
+
+namespace Pomodoro.Tests.DataAccessTests
+{
+    /// <summary>
+    /// Unit test sample class.
+    /// </summary>
+    public class UserRepositoryTests
+    {
+        /// <summary>
+        /// Adds user to database.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> object that represents an asynchronous operation.</returns>
+        [Fact]
+        public async Task AddAsync_AddsUserToDatabase()
+        {
+            // arrange
+            using var context = new AppDbContext(UnitTestHelper.GetUnitTestDbOptions());
+            var userRepository = new UserRepository(context);
+            var user = new AppUser
+            {
+                Id = new Guid(3, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                Name = "Jane",
+                Email = "jane@gmail.com",
+            };
+
+            // act
+            await userRepository.AddAsync(user);
+            await context.SaveChangesAsync();
+
+            // assert
+            Assert.Equal(3, context.AppUsers.Count());
+        }
+
+        /// <summary>
+        /// Adds users to database.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> object that represents an asynchronous operation.</returns>
+        [Fact]
+        public async Task AddRangeAsync_AddsUsersToDatabase()
+        {
+            // arrange
+            using var context = new AppDbContext(UnitTestHelper.GetUnitTestDbOptions());
+            var userRepository = new UserRepository(context);
+            var users = new List<AppUser>
+            {
+                new AppUser
+                {
+                    Id = new Guid(3, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    Name = "Jane",
+                    Email = "jane@gmail.com",
+                },
+                new AppUser
+                {
+                    Id = new Guid(4, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    Name = "Bob",
+                    Email = "bob@gmail.com",
+                },
+            };
+
+            // act
+            await userRepository.AddRangeAsync(users);
+            await context.SaveChangesAsync();
+
+            // assert
+            Assert.Equal(4, context.AppUsers.Count());
+        }
+
+        /// <summary>
+        /// Finds users.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> object that represents an asynchronous operation.</returns>
+        [Fact]
+        public async Task FindAsync_FindsUsers()
+        {
+            // arrange
+            using var context = new AppDbContext(UnitTestHelper.GetUnitTestDbOptions());
+            var userRepository = new UserRepository(context);
+            var expUsers = new List<AppUser>
+            {
+                new AppUser
+                {
+                    Id = new Guid(1, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    Name = "Viktor",
+                    Email = "vitia@gmail.com",
+                },
+                new AppUser
+                {
+                    Id = new Guid(2, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    Name = "Mia",
+                    Email = "mia@gmail.com",
+                },
+            };
+
+            // act
+            var actUsers = await userRepository.FindAsync(x => x.Name == expUsers[0].Name
+                                                            || x.Name == expUsers[1].Name);
+
+            // assert
+            Assert.Equal(expUsers, actUsers, new AppUserComparer());
+        }
+
+        /// <summary>
+        /// Does not find any user.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> object that represents an asynchronous operation.</returns>
+        [Fact]
+        public async Task FindAsync_DoesNotFindAnyUser()
+        {
+            // arrange
+            using var context = new AppDbContext(UnitTestHelper.GetUnitTestDbOptions());
+            var userRepository = new UserRepository(context);
+
+            // act
+            var actUser = await userRepository.FindAsync(u => u.Name == "Will8795");
+
+            // assert
+            Assert.Empty(actUser);
+        }
+
+        /// <summary>
+        /// Returns all users.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> object that represents an asynchronous operation.</returns>
+        [Fact]
+        public async Task GetAllAsync_ReturnsAllUsers()
+        {
+            // arrange
+            using var context = new AppDbContext(UnitTestHelper.GetUnitTestDbOptions());
+            var userRepository = new UserRepository(context);
+            var expUsers = new List<AppUser>
+            {
+                new AppUser
+                {
+                    Id = new Guid(1, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    Name = "Viktor",
+                    Email = "vitia@gmail.com",
+                },
+                new AppUser
+                {
+                    Id = new Guid(2, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    Name = "Mia",
+                    Email = "mia@gmail.com",
+                },
+            };
+
+            // act
+            var actUsers = await userRepository.GetAllAsync();
+
+            // assert
+            Assert.Equal(expUsers, actUsers, new AppUserComparer());
+        }
+
+        /// <summary>
+        /// Gets user by id.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> object that represents an asynchronous operation.</returns>
+        [Fact]
+        public async Task GetByIdAsync_ReturnsUsers()
+        {
+            // arrange
+            using var context = new AppDbContext(UnitTestHelper.GetUnitTestDbOptions());
+            var userRepository = new UserRepository(context);
+            var expUser = new AppUser
+            {
+                Id = new Guid(1, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                Name = "Viktor",
+                Email = "vitia@gmail.com",
+            };
+
+            // act
+            var actUses = await userRepository.GetByIdAsync(expUser.Id);
+
+            // assert
+            Assert.Equal(expUser, actUses, new AppUserComparer());
+        }
+
+        /// <summary>
+        /// Remove user from db.
+        /// </summary>
+        [Fact]
+        public void Remove_RemovesUser()
+        {
+            // arrange
+            using var context = new AppDbContext(UnitTestHelper.GetUnitTestDbOptions());
+            var userRepository = new UserRepository(context);
+            var user = new AppUser
+            {
+                Id = new Guid(1, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                Name = "Viktor",
+                Email = "vitia@gmail.com",
+            };
+
+            // act
+            userRepository.Remove(user);
+            context.SaveChanges();
+
+            // assert
+            Assert.Equal(1, context.AppUsers.Count());
+        }
+
+        /// <summary>
+        /// Removes users from db.
+        /// </summary>
+        [Fact]
+        public void RemoveRange_RemovesUsers()
+        {
+            // arrange
+            using var context = new AppDbContext(UnitTestHelper.GetUnitTestDbOptions());
+            var userRepository = new UserRepository(context);
+            var users = new List<AppUser>
+            {
+                new AppUser
+                {
+                    Id = new Guid(1, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    Name = "Viktor",
+                    Email = "vitia@gmail.com",
+                },
+                new AppUser
+                {
+                    Id = new Guid(2, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    Name = "Mia",
+                    Email = "mia@gmail.com",
+                },
+            };
+
+            // act
+            userRepository.RemoveRange(users);
+            context.SaveChanges();
+
+            // assert
+            Assert.Empty(context.AppUsers);
+        }
+
+        /// <summary>
+        /// Updates user.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> object that represents an asynchronous operation.</returns>
+        [Fact]
+        public async Task Update_UpdatesUser()
+        {
+            // arrange
+            using var context = new AppDbContext(UnitTestHelper.GetUnitTestDbOptions());
+            var userRepository = new UserRepository(context);
+            var expUser = new AppUser
+            {
+                Id = new Guid(1, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                Name = "ViktorNew",
+                Email = "vitianew@gmail.com",
+            };
+
+            // act
+            userRepository.Update(expUser);
+            context.SaveChanges();
+            var actUses = await userRepository.GetByIdAsync(expUser.Id);
+
+            // assert
+            Assert.Equal(expUser, actUses, new AppUserComparer());
+        }
+    }
+}
---
+++ Pomodoro.Tests/UnitTestHelper.cs
@@ -0,0 +1,246 @@
+﻿// <copyright file="UnitTestHelper.cs" company="PomodoroGroup_GL_BaseCamp">
+// Copyright (c) PomodoroGroup_GL_BaseCamp. All rights reserved.
+// </copyright>
+
+using Microsoft.EntityFrameworkCore;
+using Pomodoro.DataAccess.EF;
+using Pomodoro.DataAccess.Entities;
+using Pomodoro.DataAccess.Enums;
+
+namespace Pomodoro.Tests
+{
+    /// <summary>
+    /// Class that provide parts for tests arranging.
+    /// </summary>
+    public static class UnitTestHelper
+    {
+        /// <summary>
+        /// Create DbContext options.
+        /// </summary>
+        /// <returns>DbContext.</returns>
+        public static DbContextOptions<AppDbContext> GetUnitTestDbOptions()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            using (var context = new AppDbContext(options))
+            {
+                SeedData(context);
+                context.SaveChanges();
+            }
+
+            return options;
+        }
+
+        private static void SeedData(AppDbContext context)
+        {
+            SeedFrequencyTypes(context);
+            SeedFrequencies(context);
+            SeedTasks(context);
+            SeedCompletedTasks(context);
+            SeedAppUsers(context);
+            SeedSettings(context);
+        }
+
+        private static void SeedFrequencyTypes(AppDbContext context)
+        {
+            context.FrequencyTypes.AddRange(
+                new FrequencyType
+                {
+                    Id = new Guid(1, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    Value = FrequencyValue.None,
+                },
+                new FrequencyType
+                {
+                    Id = new Guid(2, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    Value = FrequencyValue.Day,
+                },
+                new FrequencyType
+                {
+                    Id = new Guid(3, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    Value = FrequencyValue.Week,
+                },
+                new FrequencyType
+                {
+                    Id = new Guid(4, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    Value = FrequencyValue.Month,
+                },
+                new FrequencyType
+                {
+                    Id = new Guid(5, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    Value = FrequencyValue.Year,
+                },
+                new FrequencyType
+                {
+                    Id = new Guid(6, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    Value = FrequencyValue.Workday,
+                },
+                new FrequencyType
+                {
+                    Id = new Guid(7, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    Value = FrequencyValue.Weekend,
+                });
+        }
+
+        private static void SeedFrequencies(AppDbContext context)
+        {
+            context.Frequencies.AddRange(
+                new Frequency
+                {
+                    Id = new Guid(2, 1, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    FrequencyTypeId = new Guid(1, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    Every = 0,
+                },
+                new Frequency
+                {
+                    Id = new Guid(2, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    FrequencyTypeId = new Guid(2, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    Every = 1,
+                },
+                new Frequency
+                {
+                    Id = new Guid(2, 3, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    FrequencyTypeId = new Guid(3, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    Every = 1,
+                },
+                new Frequency
+                {
+                    Id = new Guid(2, 4, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    FrequencyTypeId = new Guid(4, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    Every = 1,
+                },
+                new Frequency
+                {
+                    Id = new Guid(2, 5, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    FrequencyTypeId = new Guid(5, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    Every = 1,
+                },
+                new Frequency
+                {
+                    Id = new Guid(2, 6, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    FrequencyTypeId = new Guid(6, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    Every = 1,
+                },
+                new Frequency
+                {
+                    Id = new Guid(2, 7, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    FrequencyTypeId = new Guid(7, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    Every = 1,
+                });
+        }
+
+        private static void SeedTasks(AppDbContext context)
+        {
+            context.Tasks.AddRange(
+                new TaskEntity
+                {
+                    Id = new Guid(1, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    UserId = new Guid(1, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    FrequencyId = new Guid(1, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    Title = "Cleaning",
+                    InitialDate = new DateTime(2023, 1, 10),
+                    AllocatedTime = 3600,
+                },
+                new TaskEntity
+                {
+                    Id = new Guid(2, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    UserId = new Guid(1, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    FrequencyId = new Guid(2, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    Title = "Play guitar",
+                    InitialDate = new DateTime(2023, 1, 11),
+                    AllocatedTime = 2000,
+                });
+        }
+
+        private static void SeedCompletedTasks(AppDbContext context)
+        {
+            context.CompletedTasks.AddRange(
+                new Completed
+                {
+                    Id = new Guid(1, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    TaskId = new Guid(1, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    ActualDate = new DateTime(2023, 1, 10),
+                    TimeSpent = 3000,
+                    PomodorosCount = 2,
+                    IsDone = true,
+                },
+                new Completed
+                {
+                    Id = new Guid(2, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    TaskId = new Guid(1, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    ActualDate = new DateTime(2023, 1, 11),
+                    TimeSpent = 4500,
+                    PomodorosCount = 3,
+                    IsDone = true,
+                },
+                new Completed
+                {
+                    Id = new Guid(3, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    TaskId = new Guid(1, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    ActualDate = new DateTime(2023, 1, 12),
+                    TimeSpent = 3000,
+                    PomodorosCount = 2,
+                    IsDone = true,
+                },
+                new Completed
+                {
+                    Id = new Guid(4, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    TaskId = new Guid(2, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    ActualDate = new DateTime(2023, 1, 11),
+                    TimeSpent = 3000,
+                    PomodorosCount = 2,
+                    IsDone = true,
+                },
+                new Completed
+                {
+                    Id = new Guid(5, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    TaskId = new Guid(2, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    ActualDate = new DateTime(2023, 1, 13),
+                    TimeSpent = 1500,
+                    PomodorosCount = 1,
+                    IsDone = true,
+                });
+        }
+
+        private static void SeedAppUsers(AppDbContext context)
+        {
+            context.AppUsers.AddRange(
+                new AppUser
+                {
+                    Id = new Guid(1, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    Name = "Viktor",
+                    Email = "vitia@gmail.com",
+                },
+                new AppUser
+                {
+                    Id = new Guid(2, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    Name = "Mia",
+                    Email = "mia@gmail.com",
+                });
+        }
+
+        private static void SeedSettings(AppDbContext context)
+        {
+            context.Settings.AddRange(
+                new Settings
+                {
+                    Id = new Guid(1, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    UserId = new Guid(1, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    PomodoroDuration = 25,
+                    ShortBreak = 5,
+                    LongBreak = 15,
+                    PomodorosBeforeLongBreak = 4,
+                },
+                new Settings
+                {
+                    Id = new Guid(2, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    UserId = new Guid(2, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    PomodoroDuration = 20,
+                    ShortBreak = 5,
+                    LongBreak = 10,
+                    PomodorosBeforeLongBreak = 3,
+                });
+        }
+    }
+}
