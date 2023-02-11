deb6720c10d09efa304ef332ce7b279bc5d46a9c
unknown <asatrian.v@gmail.com>
2023-02-11 16:37:36 +0200

    add frequency type repository tests

---
+++ Pomodoro.Tests/DataAccessTests/FrequencyTypeRepositoryTests.cs
@@ -0,0 +1,194 @@
+﻿// <copyright file="FrequencyTypeRepositoryTests.cs" company="PomodoroGroup_GL_BaseCamp">
+// Copyright (c) PomodoroGroup_GL_BaseCamp. All rights reserved.
+// </copyright>
+
+using Pomodoro.DataAccess.EF;
+using Pomodoro.DataAccess.Entities;
+using Pomodoro.DataAccess.Enums;
+using Pomodoro.DataAccess.Repositories.Realizations;
+using Pomodoro.Tests.EqualityComparers;
+
+namespace Pomodoro.Tests.DataAccessTests
+{
+    /// <summary>
+    /// FrequencyType repository test class.
+    /// </summary>
+    public class FrequencyTypeRepositoryTests
+    {
+        /// <summary>
+        /// Adds frequency type to database.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> object that represents an asynchronous operation.</returns>
+        [Fact]
+        public async Task AddAsync_AddsFrequencyTypeToDatabase()
+        {
+            // arrange
+            using var context = new AppDbContext(UnitTestHelper.GetUnitTestDbOptions());
+            var frequencyTypeRepository = new FrequencyTypeRepository(context);
+            var frequencyType = new FrequencyType
+            {
+                Id = new Guid(8, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                Value = FrequencyValue.Month,
+            };
+
+            // act
+            await frequencyTypeRepository.AddAsync(frequencyType);
+            await context.SaveChangesAsync();
+
+            // assert
+            Assert.Equal(8, context.FrequencyTypes.Count());
+        }
+
+        /// <summary>
+        /// Adds frequency types to database.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> object that represents an asynchronous operation.</returns>
+        [Fact]
+        public async Task AddRangeAsync_AddsFrequencyTypesToDatabase()
+        {
+            // arrange
+            using var context = new AppDbContext(UnitTestHelper.GetUnitTestDbOptions());
+            var frequencyTypeRepository = new FrequencyTypeRepository(context);
+            var frequencyTypes = new List<FrequencyType>
+            {
+                new FrequencyType
+                {
+                    Id = new Guid(8, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    Value = FrequencyValue.Month,
+                },
+                new FrequencyType
+                {
+                    Id = new Guid(9, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                    Value = FrequencyValue.Year,
+                },
+            };
+
+            // act
+            await frequencyTypeRepository.AddRangeAsync(frequencyTypes);
+            await context.SaveChangesAsync();
+
+            // assert
+            Assert.Equal(9, context.FrequencyTypes.Count());
+        }
+
+        /// <summary>
+        /// Finds frequency types.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> object that represents an asynchronous operation.</returns>
+        [Fact]
+        public async Task FindAsync_FindsFrequencyTypes()
+        {
+            // arrange
+            using var context = new AppDbContext(UnitTestHelper.GetUnitTestDbOptions());
+            var frequencyTypeRepository = new FrequencyTypeRepository(context);
+            var expFrequencyTypes = context.FrequencyTypes.Take(1).ToList();
+
+            // act
+            var actFrequencyTypes = await frequencyTypeRepository.FindAsync(x => x.Value == expFrequencyTypes[0].Value);
+
+            // assert
+            Assert.Equal(expFrequencyTypes, actFrequencyTypes, new FrequencyTypeComparer());
+        }
+
+        /// <summary>
+        /// Returns all frequency types.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> object that represents an asynchronous operation.</returns>
+        [Fact]
+        public async Task GetAllAsync_ReturnsAllFrequencyTypes()
+        {
+            // arrange
+            using var context = new AppDbContext(UnitTestHelper.GetUnitTestDbOptions());
+            var frequencyTypeRepository = new FrequencyTypeRepository(context);
+            var expFrequencyTypes = context.FrequencyTypes.ToList();
+
+            // act
+            var actFrequencyTypes = await frequencyTypeRepository.GetAllAsync();
+
+            // assert
+            Assert.Equal(expFrequencyTypes, actFrequencyTypes, new FrequencyTypeComparer());
+        }
+
+        /// <summary>
+        /// Gets frequency type by id.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> object that represents an asynchronous operation.</returns>
+        [Fact]
+        public async Task GetByIdAsync_ReturnsFrequencyType()
+        {
+            // arrange
+            using var context = new AppDbContext(UnitTestHelper.GetUnitTestDbOptions());
+            var frequencyTypeRepository = new FrequencyTypeRepository(context);
+            var expFrequencyType = context.FrequencyTypes.First();
+
+            // act
+            var actFrequencyType = await frequencyTypeRepository.GetByIdAsync(expFrequencyType.Id);
+
+            // assert
+            Assert.Equal(expFrequencyType, actFrequencyType, new FrequencyTypeComparer());
+        }
+
+        /// <summary>
+        /// Remove frequency type from db.
+        /// </summary>
+        [Fact]
+        public void Remove_RemovesFrequencyType()
+        {
+            // arrange
+            using var context = new AppDbContext(UnitTestHelper.GetUnitTestDbOptions());
+            var frequencyTypeRepository = new FrequencyTypeRepository(context);
+            var frequencyType = context.FrequencyTypes.First();
+
+            // act
+            frequencyTypeRepository.Remove(frequencyType);
+            context.SaveChanges();
+
+            // assert
+            Assert.Equal(6, context.FrequencyTypes.Count());
+        }
+
+        /// <summary>
+        /// Removes frequency types from db.
+        /// </summary>
+        [Fact]
+        public void RemoveRange_RemovesFrequencyTypes()
+        {
+            // arrange
+            using var context = new AppDbContext(UnitTestHelper.GetUnitTestDbOptions());
+            var frequencyTypeRepository = new FrequencyTypeRepository(context);
+            var frequencyTypes = context.FrequencyTypes.ToList();
+
+            // act
+            frequencyTypeRepository.RemoveRange(frequencyTypes);
+            context.SaveChanges();
+
+            // assert
+            Assert.Empty(context.FrequencyTypes);
+        }
+
+        /// <summary>
+        /// Updates frequency type.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> object that represents an asynchronous operation.</returns>
+        [Fact]
+        public async Task Update_UpdatesFrequencyType()
+        {
+            // arrange
+            using var context = new AppDbContext(UnitTestHelper.GetUnitTestDbOptions());
+            var frequencyTypeRepository = new FrequencyTypeRepository(context);
+            var expFrequency = new FrequencyType
+            {
+                Id = new Guid(7, 2, 3, new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }),
+                Value = FrequencyValue.Year,
+            };
+
+            // act
+            frequencyTypeRepository.Update(expFrequency);
+            context.SaveChanges();
+            var actFrequencyType = await frequencyTypeRepository.GetByIdAsync(expFrequency.Id);
+
+            // assert
+            Assert.Equal(expFrequency, actFrequencyType, new FrequencyTypeComparer());
+        }
+    }
+}
---
+++ Pomodoro.Tests/EqualityComparers/FrequencyTypeComparer.cs
@@ -0,0 +1,38 @@
+﻿// <copyright file="FrequencyTypeComparer.cs" company="PomodoroGroup_GL_BaseCamp">
+// Copyright (c) PomodoroGroup_GL_BaseCamp. All rights reserved.
+// </copyright>
+
+using System.Diagnostics.CodeAnalysis;
+using Pomodoro.DataAccess.Entities;
+
+namespace Pomodoro.Tests.EqualityComparers
+{
+    /// <summary>
+    /// Frequency for Task.
+    /// </summary>
+    public class FrequencyTypeComparer : IEqualityComparer<FrequencyType?>
+    {
+        /// <inheritdoc/>
+        public bool Equals([AllowNull] FrequencyType x, [AllowNull] FrequencyType y)
+        {
+            if (x == null && y == null)
+            {
+                return true;
+            }
+
+            if (x == null || y == null)
+            {
+                return false;
+            }
+
+            return x.Id == y.Id
+                && x.Value == y.Value;
+        }
+
+        /// <inheritdoc/>
+        public int GetHashCode([DisallowNull] FrequencyType obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+}
