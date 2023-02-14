128f4bf8d3eee6c2ce010988517215f4bdec2366
Сігута Діана <diana.sihuta@nure.ua>
2023-02-14 19:40:06 +0200

    Add automapper configs for settings models

---
+++ Pomodoro.Api/Mapping/SettingsProfile.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="SettingsProfile.cs" company="PomodoroGroup_GL_BaseCamp">
+// Copyright (c) PomodoroGroup_GL_BaseCamp. All rights reserved.
+// </copyright>
+
+using AutoMapper;
+using Pomodoro.Api.ViewModels;
+using Pomodoro.Core.Models;
+
+namespace Pomodoro.Api.Mapping
+{
+    /// <summary>
+    /// Provides mapping configuration for models which represent pomodoro settings.
+    /// </summary>
+    public class SettingsProfile : Profile
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SettingsProfile"/> class.
+        /// </summary>
+        public SettingsProfile()
+        {
+            this.CreateMap<SettingsModel, SettingsViewModel>()
+                .ReverseMap();
+        }
+    }
+}
