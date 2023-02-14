8d873b7c3c61ccd73398c096db7aacb3b8444770
Сігута Діана <diana.sihuta@nure.ua>
2023-02-14 19:37:58 +0200

    Add ISettingsService

---
+++ Pomodoro.Core/Interfaces/IServices/ISettingsService.cs
@@ -0,0 +1,12 @@
+﻿using Pomodoro.Core.Models;
+
+namespace Pomodoro.Core.Interfaces.IServices
+{
+    public interface ISettingsService
+    {
+        Task<SettingsModel?> GetSettingsAsync(Guid id);
+        Task<SettingsModel?> GetUserSettingsAsync(Guid userId);
+        Task<SettingsModel> CreateSettingsAsync(SettingsModel settingsModel);
+        Task<SettingsModel> UpdateSettingsAsync(SettingsModel settingsModel);
+    }
+}
