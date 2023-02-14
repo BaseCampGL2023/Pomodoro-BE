8a117099db4dfb527d76b1392b19b4f946052fe7
Сігута Діана <diana.sihuta@nure.ua>
2023-02-14 19:36:08 +0200

    Add validation attributes to the settings view model.

--- Pomodoro.Api/ViewModels/SettingsViewModel.cs
+++ Pomodoro.Api/ViewModels/SettingsViewModel.cs
@@ -2,6 +2,7 @@
 // Copyright (c) PomodoroGroup_GL_BaseCamp. All rights reserved.
 // </copyright>
 
+using System.ComponentModel.DataAnnotations;
 using Pomodoro.Api.ViewModels.Base;
 
 namespace Pomodoro.Api.ViewModels
@@ -19,21 +20,25 @@ namespace Pomodoro.Api.ViewModels
         /// <summary>
         /// Gets or sets duration (in minutes) of the pomodoro timer.
         /// </summary>
+        [Range(1, byte.MaxValue, ErrorMessage = "The {0} property must be in the range from {1} to {2}.")]
         public byte PomodoroDuration { get; set; }
 
         /// <summary>
         /// Gets or sets duration (in minutes) of the short break between pomodoros.
         /// </summary>
+        [Range(1, byte.MaxValue, ErrorMessage = "The {0} property must be in the range from {1} to {2}.")]
         public byte ShortBreak { get; set; }
 
         /// <summary>
         /// Gets or sets duration (in minutes) of the long break between pomodoros.
         /// </summary>
+        [Range(1, byte.MaxValue, ErrorMessage = "The {0} property must be in the range from {1} to {2}.")]
         public byte LongBreak { get; set; }
 
         /// <summary>
         /// Gets or sets number of pomodoros after which the long break starts.
         /// </summary>
+        [Range(1, byte.MaxValue, ErrorMessage = "The {0} property must be in the range from {1} to {2}.")]
         public byte PomodorosBeforeLongBreak { get; set; }
 
         /// <summary>
