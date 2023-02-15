b7519508f8fcf578ee5b99393e50033d4a91fcd6
Kostia <you@example.com>
2023-02-15 23:06:41 +0200

    Removed .idea and changed implementation of IStatisticsService

--- Pomodoro.Services/StatisticsService.cs
+++ Pomodoro.Services/StatisticsService.cs
@@ -1,110 +1,51 @@
-﻿using Pomodoro.Core.Enums;
-using Pomodoro.Core.Interfaces.IServices;
+﻿using Pomodoro.Core.Interfaces.IServices;
 using Pomodoro.Core.Models.Statistics;
-using Pomodoro.DataAccess.EF;
-using Pomodoro.DataAccess.Entities;
-
+using Pomodoro.DataAccess.Repositories.Interfaces;
+using AutoMapper;
 namespace Pomodoro.Services
 {
-	public  class StatisticsService : IStatisticsService
-	{
-		public async Task<DailyStatistics> GetDailyStatisticsAsync(Guid userId, DateOnly day)
-		{
-			DailyStatistics dailyStatistics = new DailyStatistics();
+    public class StatisticsService:IStatisticsService
+    {
+        private readonly IMapper _mapper;
+        private readonly ICompletedRepository _completedRepository;
 
-			using (AppDbContext db = new AppDbContext())
-			{
-				IQueryable<Completed> tasks = db.CompletedTasks;
-				
-				tasks = tasks.Where(t => t.Id == userId);
-				tasks = tasks.Where(t => t.ActualDate.Year == day.Year);
-				tasks = tasks.Where(t => t.ActualDate.Month == day.Month);
-				tasks = tasks.Where(t => t.ActualDate.Day == day.Day);
+        public StatisticsService(IMapper mapper, ICompletedRepository repository)
+        {
+            this._mapper = mapper;
+            this._completedRepository = repository;
+        }
+        
+        public async Task<DailyStatistics> GetDailyStatisticsAsync(Guid userId, DateOnly day)
+        {
+            var dailystatistics = await _completedRepository.FindAsync(
+                u=>u.Task != null &&
+                   u.Task.UserId == userId &&
+                   u.ActualDate.Year == day.Year &&
+                   u.ActualDate.Month == day.Month && 
+                   u.ActualDate.Day == day.Day
+            );
+            return  _mapper.Map<DailyStatistics>(dailystatistics);
+        }
 
+        public async Task<MonthlyStatistics> GetMonthlyStatisticsAsync(Guid userId, int year, int month)
+        {
+            var monthlystatistics = await _completedRepository.FindAsync(
+                m=> m.Task != null &&
+                    m.Task.UserId == userId && 
+                    m.ActualDate.Year == year && 
+                    m.ActualDate.Month == month );
 
-				for (int i = 0; i < 24; i++)
-				{
-					int hour = i;
-					
-					AnalyticsPerHour analytics = new AnalyticsPerHour();
+            return _mapper.Map<MonthlyStatistics>(monthlystatistics);
+        }
 
-					IQueryable<Completed> hourstatistic = tasks.Where(t => t.ActualDate.Hour == hour);
-
-					analytics.Hour = hour;
-
-					foreach (var h in hourstatistic)
-					{
-						analytics.TimeSpent += h.TimeSpent;
-						analytics.PomodorosDone += h.PomodorosCount;
-					}
-
-					dailyStatistics.AnalyticsPerHours?.Add(analytics);
-				}
-			}
-			return await Task.FromResult(dailyStatistics);
-
-		}
-
-		public async Task<MonthlyStatistics> GetMonthlyStatisticsAsync(Guid userId, int year, int month)
-		{
-			MonthlyStatistics monthlyStatistics = new MonthlyStatistics();
-
-			monthlyStatistics.UserId = userId;
-
-			using (AppDbContext db = new AppDbContext())
-			{
-				IQueryable<Completed> tasks = db.CompletedTasks;
-				
-				tasks = tasks.Where(t => t.Id == userId);
-				tasks = tasks.Where(t => t.ActualDate.Year == year);
-				tasks = tasks.Where(t => t.ActualDate.Month == month);
-
-				monthlyStatistics.TasksCompleted = tasks.Count();
-
-				foreach (var pomo in tasks)
-				{
-					monthlyStatistics.PomodorosDone += pomo.PomodorosCount;
-				}
-				foreach (var time in tasks)
-				{
-					monthlyStatistics.TimeSpent += time.TimeSpent;
-				}
-			}
-
-			return await Task.FromResult(monthlyStatistics);
-
-		}
-
-		public async Task<AnnualStatistics> GetAnnualStatisticsAsync(Guid userId, int year)
-		{
-			AnnualStatistics annualStatistics = new AnnualStatistics();
-
-			annualStatistics.UserId = userId;
-			annualStatistics.Year = year;
-
-			using (AppDbContext db = new AppDbContext())
-			{
-				IQueryable<Completed> monthsTasks = db.CompletedTasks;
-
-				foreach (int m in Enum.GetValues(typeof(Month)))
-				{
-					monthsTasks = monthsTasks.Where(t => t.ActualDate.Year == annualStatistics.Year);
-					monthsTasks = monthsTasks.Where(t => t.ActualDate.Month == m);
-
-					AnalyticsPerMonth temp = new AnalyticsPerMonth();
-					
-					temp.Month = (Month)m;
-					foreach (var d in monthsTasks)
-					{
-						temp.PomodorosDone += d.PomodorosCount;
-					}
-					annualStatistics.AnalyticsPerMonths?.Add(temp);
-				}
-
-			}
-
-			return await Task.FromResult(annualStatistics);
-
-		}
-	}
+        public async Task<AnnualStatistics> GetAnnualStatisticsAsync(Guid userId, int year)
+        {
+            var annualstatistics = await _completedRepository.FindAsync(
+                a => a.Task != null &&
+                     a.Task.UserId == userId && 
+                     a.ActualDate.Year == year
+            );
+            return _mapper.Map<AnnualStatistics>(annualstatistics);
+        }
+    }
 }
