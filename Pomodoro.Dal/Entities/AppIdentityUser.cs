245f49dff77e9932448dc57ba163cd1769d4a757
Anatolii Fedorchenko <anatoliyf89@gmail.com>
2023-03-09 12:58:10 +0200

    Added User classes

---
+++ Pomodoro.Dal/Entities/AppIdentityUser.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="AppIdentityUser.cs" company="PomodoroGroup_GL_BaseCamp">
+// Copyright (c) PomodoroGroup_GL_BaseCamp. All rights reserved.
+// </copyright>
+
+using Microsoft.AspNetCore.Identity;
+using Pomodoro.Dal.Entities.Base;
+
+namespace Pomodoro.Dal.Entities
+{
+    /// <summary>
+    /// Represent user in authentication system.
+    /// </summary>
+    public class AppIdentityUser : IdentityUser<Guid>, IEntity
+    {
+        /// <summary>
+        /// Gets or sets navigation property represents application user.
+        /// </summary>
+        public AppUser? AppUser { get; set; }
+    }
+}
---
+++ Pomodoro.Dal/Entities/AppUser.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="AppUser.cs" company="PomodoroGroup_GL_BaseCamp">
+// Copyright (c) PomodoroGroup_GL_BaseCamp. All rights reserved.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pomodoro.Dal.Entities.Base;
+
+namespace Pomodoro.Dal.Entities
+{
+    /// <summary>
+    /// Represent application user.
+    /// </summary>
+    public class AppUser : BaseEntity
+    {
+        /// <summary>
+        /// Gets or sets username property.
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets IdentityUser foreign key.
+        /// </summary>
+        public Guid IdentityUserId { get; set; }
+
+        /// <summary>
+        /// Gets or sets navigation property represents user in authentication system.
+        /// </summary>
+        public AppIdentityUser? AppIdentityUser { get; set; }
+    }
+}
