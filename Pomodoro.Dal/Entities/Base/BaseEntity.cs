44773c900d020001a647ec0df844ba75aae7878c
Anatolii Fedorchenko <anatoliyf89@gmail.com>
2023-03-09 12:44:40 +0200

    Added BaseEntity interface and class

---
+++ Pomodoro.Dal/Entities/Base/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="BaseEntity.cs" company="PomodoroGroup_GL_BaseCamp">
+// Copyright (c) PomodoroGroup_GL_BaseCamp. All rights reserved.
+// </copyright>
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pomodoro.Dal.Entities.Base
+{
+    /// <summary>
+    /// Abstract class, define type: entities with Guid primary key.
+    /// </summary>
+    public abstract class BaseEntity : IEntity
+    {
+        /// <inheritdoc/>
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+    }
+}
---
+++ Pomodoro.Dal/Entities/Base/IEntity.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="IEntity.cs" company="PomodoroGroup_GL_BaseCamp">
+// Copyright (c) PomodoroGroup_GL_BaseCamp. All rights reserved.
+// </copyright>
+
+namespace Pomodoro.Dal.Entities.Base
+{
+    /// <summary>
+    /// Define type: entities with Guid primary key.
+    /// </summary>
+    public interface IEntity
+    {
+        /// <summary>
+        /// Gets or sets Id property.
+        /// </summary>
+        public Guid Id { get; set; }
+    }
+}
