6fabe5d152fd9a0c1b0e39d4642c1cb389ef1caa
Anatolii Fedorchenko <anatoliyf89@gmail.com>
2023-03-09 23:00:19 +0200

    Added AppDbContextFactory

---
+++ Pomodoro.Dal/Data/AppDbContextFactory.cs
@@ -0,0 +1,29 @@
+﻿// <copyright file="AppDbContextFactory.cs" company="PomodoroGroup_GL_BaseCamp">
+// Copyright (c) PomodoroGroup_GL_BaseCamp. All rights reserved.
+// </copyright>
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Pomodoro.Dal.Data
+{
+    /// <summary>
+    /// Design time context factory.
+    /// </summary>
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        /// <summary>
+        /// Create instance of DbContext class.
+        /// </summary>
+        /// <param name="args">Arguments from command line.</param>
+        /// <returns>AppDbContext instance <see cref="AppDbContext"/>.</returns>
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            var connectionString = @"Server=(localdb)\mssqllocaldb;Database=Pomodoro_Ex;Trusted_Connection=True;";
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
