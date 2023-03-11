2b53bbc7700bb787b63c28e64e9aa144b6428510
Anatolii Fedorchenko <anatoliyf89@gmail.com>
2023-03-11 21:48:11 +0200

    Added BaseRepository class and interface

---
+++ Pomodoro.Dal/Repositories/Base/BaseRepository.cs
@@ -0,0 +1,165 @@
+﻿// <copyright file="BaseRepository.cs" company="PomodoroGroup_GL_BaseCamp">
+// Copyright (c) PomodoroGroup_GL_BaseCamp. All rights reserved.
+// </copyright>
+
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Pomodoro.Dal.Data;
+using Pomodoro.Dal.Entities.Base;
+
+namespace Pomodoro.Dal.Repositories.Base
+{
+    /// <inheritdoc/>
+    public abstract class BaseRepository<T> : IRepository<T>
+        where T : class, IEntity, new()
+    {
+        private readonly bool disposeContext;
+        private bool isDisposed;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BaseRepository{T}"/> class when used
+        /// with DI container.
+        /// </summary>
+        /// <param name="context">Instance of AppDbContext class <see cref="AppDbContext"/>.</param>
+        protected BaseRepository(AppDbContext context)
+        {
+            this.Context = context;
+            this.Table = this.Context.Set<T>();
+            this.disposeContext = false;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BaseRepository{T}"/> class without using DI container.
+        /// </summary>
+        /// <param name="options">Instance of DbContextOptions to instantiate AppDbContext.</param>
+        protected BaseRepository(DbContextOptions<AppDbContext> options)
+            : this(new AppDbContext(options))
+        {
+            this.disposeContext = true;
+        }
+
+        /// <summary>
+        /// Finalizes an instance of the <see cref="BaseRepository{T}"/> class. It is used to match the Dispose pattern.
+        /// </summary>
+        ~BaseRepository()
+        {
+            this.Dispose(false);
+        }
+
+        /// <summary>
+        /// Gets instance of AppDbContext class <see cref="AppDbContext"/>.
+        /// </summary>
+        public AppDbContext Context { get; }
+
+        /// <summary>
+        /// Gets a DbSet property to perform specific query in client code.
+        /// </summary>
+        public DbSet<T> Table { get; }
+
+        /// <summary>
+        /// Implementing of IDisposable interface <see cref="IDisposable"/>.
+        /// </summary>
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <inheritdoc/>
+        public async Task<int> AddAsync(T entity, bool persist = false)
+        {
+            await this.Table.AddAsync(entity);
+            return persist ? await this.SaveChangesAsync() : 0;
+        }
+
+        /// <inheritdoc/>
+        public async Task<int> AddRangeAsync(IEnumerable<T> entities, bool persist = false)
+        {
+            await this.Table.AddRangeAsync(entities);
+            return persist ? await this.SaveChangesAsync() : 0;
+        }
+
+        /// <inheritdoc/>
+        public async Task<int> DeleteAsync(T entity, bool persist = false)
+        {
+            this.Table.Remove(entity);
+            return persist ? await this.SaveChangesAsync() : 0;
+        }
+
+        /// <inheritdoc/>
+        public async Task<int> DeleteAsync(Guid id, bool persist = false)
+        {
+            var entity = new T { Id = id };
+            this.Context.Entry(entity).State = EntityState.Deleted;
+            return persist ? await this.SaveChangesAsync() : 0;
+        }
+
+        /// <inheritdoc/>
+        public async Task<int> DeleteRangeAsync(IEnumerable<T> entities, bool persist = false)
+        {
+            this.Table.RemoveRange(entities);
+            return persist ? await this.SaveChangesAsync() : 0;
+        }
+
+        /// <inheritdoc/>
+        public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+            => await this.Table.Where(predicate).ToListAsync();
+
+        /// <inheritdoc/>
+        public async Task<IEnumerable<T>> GetAllAsync()
+            => await this.Table.ToListAsync();
+
+        /// <inheritdoc/>
+        public async Task<IEnumerable<T>> GetAllIgnoreQueryFilters()
+            => await this.Table.IgnoreQueryFilters().ToListAsync();
+
+        /// <inheritdoc/>
+        public async Task<T?> GetByIdAsync(int id)
+            => await this.Table.FindAsync(id);
+
+        /// <inheritdoc/>
+        public async Task<bool> IsExist(Guid id)
+            => await this.Table.AnyAsync(e => e.Id == id);
+
+        /// <inheritdoc/>
+        public async Task<int> SaveChangesAsync()
+        {
+            // TODO: Exception handling. Add Iloggger as dependency.
+            return await this.Context.SaveChangesAsync();
+        }
+
+        /// <inheritdoc/>
+        public Task<int> UpdateAsync(T entity, bool persist = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public Task<int> UpdateRangeAsync(IEnumerable<T> entities, bool persist = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Disposes repository.
+        /// </summary>
+        /// <param name="disposing">Give TRUE if it used out of DI container scope.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (this.isDisposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                if (this.disposeContext)
+                {
+                    this.Context.Dispose();
+                }
+
+                this.isDisposed = true;
+            }
+        }
+    }
+}
---
+++ Pomodoro.Dal/Repositories/Base/IRepository.cs
@@ -0,0 +1,119 @@
+﻿// <copyright file="IRepository.cs" company="PomodoroGroup_GL_BaseCamp">
+// Copyright (c) PomodoroGroup_GL_BaseCamp. All rights reserved.
+// </copyright>
+
+using System.Linq.Expressions;
+using Pomodoro.Dal.Entities.Base;
+
+namespace Pomodoro.Dal.Repositories.Base
+{
+    /// <summary>
+    /// Basic repository implementing СRUD functionality.
+    /// </summary>
+    /// <typeparam name="T">Instance of type IEntity <see cref="IEntity"/>.</typeparam>
+    public interface IRepository<T> : IDisposable
+        where T : IEntity
+    {
+        /// <summary>
+        /// Add one entity in database.
+        /// </summary>
+        /// <param name="entity">Instance of type IEntity <see cref="IEntity"/>.</param>
+        /// <param name="persist">Parameter determines whether the repository executes SaveChanges()
+        /// immediately when method are called.</param>
+        /// <returns>Number of affected entities.</returns>
+        Task<int> AddAsync(T entity, bool persist = false);
+
+        /// <summary>
+        /// Add range of entities in database.
+        /// </summary>
+        /// <param name="entities">Collection of IEntity objects <see cref="IEntity"/>.</param>
+        /// <param name="persist">Parameter determines whether the repository executes SaveChanges()
+        /// immediately when method are called.</param>
+        /// <returns>Number of affected entities.</returns>
+        Task<int> AddRangeAsync(IEnumerable<T> entities, bool persist = false);
+
+        /// <summary>
+        /// Update one entity in database.
+        /// </summary>
+        /// <param name="entity">Instance of type IEntity <see cref="IEntity"/>.</param>
+        /// <param name="persist">Parameter determines whether the repository executes SaveChanges()
+        /// immediately when method are called.</param>
+        /// <returns>Number of affected entities.</returns>
+        Task<int> UpdateAsync(T entity, bool persist = false);
+
+        /// <summary>
+        /// Update range of entities in database.
+        /// </summary>
+        /// <param name="entities">Collection of IEntity objects <see cref="IEntity"/>.</param>
+        /// <param name="persist">Parameter determines whether the repository executes SaveChanges()
+        /// immediately when method are called.</param>
+        /// <returns>Number of affected entities.</returns>
+        Task<int> UpdateRangeAsync(IEnumerable<T> entities, bool persist = false);
+
+        /// <summary>
+        /// Delete one entity from database.
+        /// </summary>
+        /// <param name="entity">Instance of type IEntity <see cref="IEntity"/>.</param>
+        /// <param name="persist">Parameter determines whether the repository executes SaveChanges()
+        /// immediately when method are called.</param>
+        /// <returns>Number of affected entities.</returns>
+        Task<int> DeleteAsync(T entity, bool persist = false);
+
+        /// <summary>
+        /// Delete one entity from database by id.
+        /// </summary>
+        /// <param name="id">Entity id.</param>
+        /// <param name="persist">Parameter determines whether the repository executes SaveChanges()
+        /// immediately when method are called.</param>
+        /// <returns>Number of affected entities.</returns>
+        Task<int> DeleteAsync(Guid id, bool persist = false);
+
+        /// <summary>
+        /// Delete range of entities from database.
+        /// </summary>
+        /// <param name="entities">Collection of IEntity objects <see cref="IEntity"/>.</param>
+        /// <param name="persist">Parameter determines whether the repository executes SaveChanges()
+        /// immediately when method are called.</param>
+        /// <returns>Number of affected entities.</returns>
+        Task<int> DeleteRangeAsync(IEnumerable<T> entities, bool persist = false);
+
+        /// <summary>
+        /// Retrive entity from database by id.
+        /// </summary>
+        /// <param name="id">Entity id.</param>
+        /// <returns>Queried object or null, if object with this id doesn't exist in database.</returns>
+        Task<T?> GetByIdAsync(int id);
+
+        /// <summary>
+        /// Retrieve all entites from database.
+        /// </summary>
+        /// <returns>IEnumerable collection of objects.</returns>
+        Task<IEnumerable<T>> GetAllAsync();
+
+        /// <summary>
+        /// Retrieve all entites from database without adding them to ChangeTracker.
+        /// </summary>
+        /// <returns>IEnumerable collection of objects.</returns>
+        Task<IEnumerable<T>> GetAllIgnoreQueryFilters();
+
+        /// <summary>
+        /// Retrieve collection of entities that satisfying predicate.
+        /// </summary>
+        /// <param name="predicate">Expression for query filtration.</param>
+        /// <returns>IEnumerable collection of objects.</returns>
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+
+        /// <summary>
+        /// Check if object with this id already exist in database.
+        /// </summary>
+        /// <param name="id">Entity id.</param>
+        /// <returns>TRUE if object exist, FALSE otherwise.</returns>
+        Task<bool> IsExist(Guid id);
+
+        /// <summary>
+        /// Persist any changes in the tracked entities.
+        /// </summary>
+        /// <returns>Number of affected entities.</returns>
+        Task<int> SaveChangesAsync();
+    }
+}
