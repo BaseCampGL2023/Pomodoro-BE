319414caee33845103b1c15d9ba578416f1c8b2a
AnatoliyF <anatoliyf89@gmail.com>
2023-03-20 22:40:10 +0200

    Added service response

---
+++ Pomodoro.Services/Models/Results/ResponseType.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="ResponseType.cs" company="PomodoroGroup_GL_BaseCamp">
+// Copyright (c) PomodoroGroup_GL_BaseCamp. All rights reserved.
+// </copyright>
+
+namespace Pomodoro.Services.Models.Results
+{
+    /// <summary>
+    /// Defines the result type.
+    /// </summary>
+    public enum ResponseType
+    {
+        /// <summary>
+        /// Execution succesfull.
+        /// </summary>
+        Ok,
+
+        /// <summary>
+        /// Entity for execution not found.
+        /// </summary>
+        NotFound,
+
+        /// <summary>
+        /// Access for execution denied.
+        /// </summary>
+        Forbid,
+
+        /// <summary>
+        /// Execution throw's exeption.
+        /// </summary>
+        Error,
+    }
+}
---
+++ Pomodoro.Services/Models/Results/ServiceResponse.cs
@@ -0,0 +1,33 @@
+﻿// <copyright file="ServiceResponse.cs" company="PomodoroGroup_GL_BaseCamp">
+// Copyright (c) PomodoroGroup_GL_BaseCamp. All rights reserved.
+// </copyright>
+
+namespace Pomodoro.Services.Models.Results
+{
+    /// <summary>
+    /// The object returned by the service method.
+    /// </summary>
+    /// <typeparam name="T">Represent result Data type.</typeparam>
+    public class ServiceResponse<T>
+    {
+        /// <summary>
+        /// Gets a value indicating whether execution was successful.
+        /// </summary>
+        public bool Success => this.Result == ResponseType.Ok;
+
+        /// <summary>
+        /// Gets or sets result of request execution.
+        /// </summary>
+        public ResponseType Result { get; set; } = ResponseType.Error;
+
+        /// <summary>
+        /// Gets or sets returned data.
+        /// </summary>
+        public T? Data { get; set; } = default;
+
+        /// <summary>
+        /// Gets or sets error message.
+        /// </summary>
+        public string Message { get; set; } = "Something went wrong";
+    }
+}
