f60edc2ca38130e84e3c7bbc9610e18c478ecfa9
unknown <asatrian.v@gmail.com>
2023-03-26 14:18:08 +0300

    remove comments from services

--- Pomodoro.Services/Mapping/FrequencyProfile.cs
+++ Pomodoro.Services/Mapping/FrequencyProfile.cs
@@ -1,23 +1,12 @@
-﻿// <copyright file="FrequencyProfile.cs" company="PomodoroGroup_GL_BaseCamp">
-// Copyright (c) PomodoroGroup_GL_BaseCamp. All rights reserved.
-// </copyright>
-
-using AutoMapper;
+﻿using AutoMapper;
 using Pomodoro.Core.Enums;
 using Pomodoro.Core.Models.Frequency;
 using Pomodoro.DataAccess.Entities;
 
 namespace Pomodoro.Services.Mapping
 {
-    /// <summary>
-    /// Provides mapping configuration for frequnecy model.
-    /// Loaded by the <see cref="AutoMapper"/>.
-    /// </summary>
     public class FrequencyProfile : Profile
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="FrequencyProfile"/> class.
-        /// </summary>
         public FrequencyProfile()
         {
             CreateMap<Frequency, FrequencyModel>()
--- Pomodoro.Services/Mapping/TaskProfile.cs
+++ Pomodoro.Services/Mapping/TaskProfile.cs
@@ -1,23 +1,12 @@
-﻿// <copyright file="TaskProfile.cs" company="PomodoroGroup_GL_BaseCamp">
-// Copyright (c) PomodoroGroup_GL_BaseCamp. All rights reserved.
-// </copyright>
-
-using AutoMapper;
+﻿using AutoMapper;
 using Pomodoro.Core.Enums;
 using Pomodoro.Core.Models.Tasks;
 using Pomodoro.DataAccess.Entities;
 
 namespace Pomodoro.Services.Mapping
 {
-    /// <summary>
-    /// Provides mapping configuration for task model.
-    /// Loaded by the <see cref="AutoMapper"/>.
-    /// </summary>
     public class TaskProfile : Profile
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="TaskProfile"/> class.
-        /// </summary>
         public TaskProfile()
         {
             CreateMap<TaskModel, TaskEntity>()
