387e1379c340adf757c06d7f4b69e6f0d27aae81
rediska-ua <maksimsheyko2012@gmail.com>
2023-03-27 08:33:24 +0300

    removed unnecessary usings

--- Pomodoro.Api/Program.cs
+++ Pomodoro.Api/Program.cs
@@ -3,7 +3,6 @@
 // </copyright>
 
 using System.Reflection;
-using System.Text.Json.Serialization;
 using Microsoft.OpenApi.Models;
 using Pomodoro.Api.ActionFilterAttributes;
 using Pomodoro.Api.Extensions;
@@ -11,10 +10,6 @@ using Pomodoro.Api.Services;
 using Pomodoro.Core.Interfaces.IServices;
 using Pomodoro.DataAccess.Extensions;
 using Pomodoro.Services.Realizations;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Serilog;
-using Serilog.Events;
-using Serilog.Sinks.MSSqlServer;
 
 var pomodoroSpecificOrigins = "_pomodoroSpecificOrigins";
 
