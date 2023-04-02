03c15a4bffaaf7db1652d33696bb3e1ea0b6bb6e
AnatoliyF <anatoliyf89@gmail.com>
2023-04-02 20:44:34 +0300

    Implement EmailSender service

---
+++ Pomodoro.Core/Exceptions/PomoMailException.cs
@@ -0,0 +1,14 @@
+﻿namespace Pomodoro.Core.Exceptions
+{
+
+    [Serializable]
+    public class PomoMailException : PomoException
+    {
+        public PomoMailException() { }
+        public PomoMailException(string message) : base(message) { }
+        public PomoMailException(string message, Exception inner) : base(message, inner) { }
+        protected PomoMailException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
